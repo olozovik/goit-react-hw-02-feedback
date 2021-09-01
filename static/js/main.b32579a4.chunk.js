7c8666de3a92b6b7bf62d48a7868a4047cc46796
Oleg Lozovik <oldisc3@gmail.com>
2021-09-01 22:05:12 +0300

    Updates

---
+++ static/js/main.b32579a4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-02-feedback"]=this["webpackJsonpgoit-react-hw-02-feedback"]||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),a=c(3),s=c.n(a),i=(c(8),c(0));var o=function(){return Object(i.jsx)("div",{className:"App",children:"Some text."})};s.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))}},[[10,1,2]]]);
+//# sourceMappingURL=main.b32579a4.chunk.js.map
